4cf5aac1598b24d72833985c857831cea95420c7
Marcellus Tavares <marcellus.tavares@liferay.com>
2014-02-06 11:55:19 -0300

    LPS-43869 Update JournalCoverter and DDMXSD rendering to account separator as a repeatable element

--- portal-impl/src/com/liferay/portlet/journal/util/JournalConverterImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/util/JournalConverterImpl.java
@@ -86,6 +86,7 @@ public class JournalConverterImpl implements JournalConverter {
 		_ddmTypesToJournalTypes.put("ddm-link-to-page", "link_to_layout");
 		_ddmTypesToJournalTypes.put("ddm-separator", "selection_break");
 		_ddmTypesToJournalTypes.put("ddm-text-html", "text_area");
+		_ddmTypesToJournalTypes.put("ddm-separator", "selection_break");
 		_ddmTypesToJournalTypes.put("select", "list");
 		_ddmTypesToJournalTypes.put("text", "text");
 		_ddmTypesToJournalTypes.put("textarea", "text_box");
@@ -227,23 +228,26 @@ public class JournalConverterImpl implements JournalConverter {
 			return;
 		}
 
-		Field ddmField = getField(
-			dynamicElementElement, ddmStructure, defaultLocale);
+		if (!ddmStructure.isFieldTransient(name)) {
+			Field ddmField = getField(
+				dynamicElementElement, ddmStructure, defaultLocale);
 
-		String fieldName = ddmField.getName();
+			String fieldName = ddmField.getName();
 
-		Field existingDDMField = ddmFields.get(fieldName);
+			Field existingDDMField = ddmFields.get(fieldName);
 
-		if (existingDDMField != null) {
-			for (Locale locale : ddmField.getAvailableLocales()) {
-				existingDDMField.addValues(locale, ddmField.getValues(locale));
+			if (existingDDMField != null) {
+				for (Locale locale : ddmField.getAvailableLocales()) {
+					existingDDMField.addValues(
+						locale, ddmField.getValues(locale));
+				}
+			}
+			else {
+				ddmFields.put(ddmField);
 			}
 		}
-		else {
-			ddmFields.put(ddmField);
-		}
 
-		updateFieldsDisplay(ddmFields, fieldName);
+		updateFieldsDisplay(ddmFields, name);
 
 		List<Element> childrenDynamicElementElements =
 			dynamicElementElement.elements("dynamic-element");
@@ -270,10 +274,6 @@ public class JournalConverterImpl implements JournalConverter {
 			Fields ddmFields, String fieldName, String parentFieldName,
 			int parentOffset)
 		throws Exception {
-
-		if (!ddmFields.contains(fieldName)) {
-			return 0;
-		}
 
 		Field fieldsDisplayField = ddmFields.get(DDMImpl.FIELDS_DISPLAY_NAME);
 
@@ -498,53 +498,6 @@ public class JournalConverterImpl implements JournalConverter {
 
 	protected void updateContentDynamicElement(
 			Element dynamicElementElement, DDMStructure ddmStructure,
-			Field ddmField, DDMFieldsCounter ddmFieldsCounter)
-		throws Exception {
-
-		String fieldName = ddmField.getName();
-
-		String fieldType = ddmStructure.getFieldType(fieldName);
-		String indexType = ddmStructure.getFieldProperty(
-			fieldName, "indexType");
-
-		String type = _ddmTypesToJournalTypes.get(fieldType);
-
-		if (type == null) {
-			type = fieldType;
-		}
-
-		dynamicElementElement.addAttribute("type", type);
-
-		dynamicElementElement.addAttribute("index-type", indexType);
-
-		for (Locale locale : ddmField.getAvailableLocales()) {
-			Element dynamicContentElement = dynamicElementElement.addElement(
-				"dynamic-content");
-
-			dynamicContentElement.addAttribute(
-				"language-id", LocaleUtil.toLanguageId(locale));
-
-			int count = ddmFieldsCounter.get(fieldName);
-
-			Serializable fieldValue = ddmField.getValue(locale, count);
-
-			if (fieldValue instanceof Date) {
-				Date valueDate = (Date)fieldValue;
-
-				fieldValue = valueDate.getTime();
-			}
-
-			String valueString = String.valueOf(fieldValue);
-
-			updateDynamicContentValue(
-				dynamicContentElement, fieldType, valueString.trim());
-		}
-
-		ddmFieldsCounter.incrementKey(fieldName);
-	}
-
-	protected void updateContentDynamicElement(
-			Element dynamicElementElement, DDMStructure ddmStructure,
 			Fields ddmFields, DDMFieldsCounter ddmFieldsCounter)
 		throws Exception {
 
@@ -573,8 +526,58 @@ public class JournalConverterImpl implements JournalConverter {
 		}
 
 		updateContentDynamicElement(
-			dynamicElementElement, ddmStructure, ddmFields.get(fieldName),
+			dynamicElementElement, ddmStructure, ddmFields, fieldName,
 			ddmFieldsCounter);
+	}
+
+	protected void updateContentDynamicElement(
+			Element dynamicElementElement, DDMStructure ddmStructure,
+			Fields ddmFields, String fieldName,
+			DDMFieldsCounter ddmFieldsCounter)
+		throws Exception {
+
+		String fieldType = ddmStructure.getFieldType(fieldName);
+		String indexType = ddmStructure.getFieldProperty(
+			fieldName, "indexType");
+
+		String type = _ddmTypesToJournalTypes.get(fieldType);
+
+		if (type == null) {
+			type = fieldType;
+		}
+
+		dynamicElementElement.addAttribute("type", type);
+
+		dynamicElementElement.addAttribute("index-type", indexType);
+
+		if (!ddmStructure.isFieldTransient(fieldName)) {
+			Field ddmField = ddmFields.get(fieldName);
+
+			for (Locale locale : ddmField.getAvailableLocales()) {
+				Element dynamicContentElement =
+					dynamicElementElement.addElement("dynamic-content");
+
+				dynamicContentElement.addAttribute(
+					"language-id", LocaleUtil.toLanguageId(locale));
+
+				int count = ddmFieldsCounter.get(fieldName);
+
+				Serializable fieldValue = ddmField.getValue(locale, count);
+
+				if (fieldValue instanceof Date) {
+					Date valueDate = (Date)fieldValue;
+
+					fieldValue = valueDate.getTime();
+				}
+
+				String valueString = String.valueOf(fieldValue);
+
+				updateDynamicContentValue(
+					dynamicContentElement, fieldType, valueString.trim());
+			}
+		}
+
+		ddmFieldsCounter.incrementKey(fieldName);
 	}
 
 	protected void updateDDMXSDDynamicElement(
