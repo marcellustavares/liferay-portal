787a92a49bb2d7e01d599b8f68c5f1577da2b2ad
Pablo Pinheiro <pablo.carvalho@liferay.com>
2014-08-21 14:51:49 -0300

    LPS-49053 Added ddm.form.field.renderers property to portal.properties

--- portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -561,6 +561,8 @@ public interface PropsKeys {
 
 	public static final String DATABASE_MYSQL_ENGINE = "database.mysql.engine";
 
+	public static final String DDM_FORM_FIELD_RENDERERS = "ddm.form.field.renderers";
+
 	public static final String DEFAULT_ADMIN_EMAIL_ADDRESS_PREFIX = "default.admin.email.address.prefix";
 
 	public static final String DEFAULT_ADMIN_FIRST_NAME = "default.admin.first.name";
