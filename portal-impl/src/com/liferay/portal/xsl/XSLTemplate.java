9ead7f4ed7addbc197c9077d4164da0722ef4f76
Marcellus Tavares <marcellus.tavares@liferay.com>
2014-12-04 19:06:10 -0300

    LPS-51814 Add new operation - getJavaScriptProcessor - to the Template interface

--- portal-impl/src/com/liferay/portal/xsl/XSLTemplate.java
+++ portal-impl/src/com/liferay/portal/xsl/XSLTemplate.java
@@ -144,6 +144,11 @@ public class XSLTemplate implements Template {
 	}
 
 	@Override
+	public String getJavaScriptProcessor() throws TemplateException {
+		throw new TemplateException("JavaScript processor is not available");
+	}
+
+	@Override
 	public String[] getKeys() {
 		Set<String> keys = _context.keySet();
 
