f92298f6ae83483282784c51ef40a6a9fbfc04d2
Renato Rego <renato.rego@liferay.com>
2015-01-14 10:52:11 -0300

    LPS-52126 - Replacement of field, fields and fieldsNamespace by ddmFormFieldValue, ddmFormValues and ddmFormValuesNamespace respectively in the definition of DDM HTML and DDM HTML FIELD taglibs. Beyond that, is being committed too the result of the execution of command "ant build-taglibs".

--- util-taglib/src/com/liferay/taglib/ddm/base/BaseHTMLTag.java
+++ util-taglib/src/com/liferay/taglib/ddm/base/BaseHTMLTag.java
@@ -42,13 +42,11 @@ public abstract class BaseHTMLTag extends com.liferay.taglib.util.IncludeTag {
 		return _classPK;
 	}
 
-	public com.liferay.portlet.dynamicdatamapping.storage.DDMFormValues
-	getDDMFormValues() {
-		
+	public com.liferay.portlet.dynamicdatamapping.storage.DDMFormValues getDdmFormValues() {
 		return _ddmFormValues;
 	}
 
-	public java.lang.String getDDMFormValuesNamespace() {
+	public java.lang.String getDdmFormValuesNamespace() {
 		return _ddmFormValuesNamespace;
 	}
 
@@ -86,18 +84,13 @@ public abstract class BaseHTMLTag extends com.liferay.taglib.util.IncludeTag {
 		setScopedAttribute("classPK", classPK);
 	}
 
-	public void setDDMFormValues(
-			com.liferay.portlet.dynamicdatamapping.storage.DDMFormValues
-			ddmFormValues) {
-		
+	public void setDdmFormValues(com.liferay.portlet.dynamicdatamapping.storage.DDMFormValues ddmFormValues) {
 		_ddmFormValues = ddmFormValues;
 
 		setScopedAttribute("ddmFormValues", ddmFormValues);
 	}
 
-	public void setDDMFormValuesNamespace(
-		java.lang.String ddmFormValuesNamespace) {
-		
+	public void setDdmFormValuesNamespace(java.lang.String ddmFormValuesNamespace) {
 		_ddmFormValuesNamespace = ddmFormValuesNamespace;
 
 		setScopedAttribute("ddmFormValuesNamespace", ddmFormValuesNamespace);
@@ -158,13 +151,11 @@ public abstract class BaseHTMLTag extends com.liferay.taglib.util.IncludeTag {
 		setNamespacedAttribute(request, "classNameId", _classNameId);
 		setNamespacedAttribute(request, "classPK", _classPK);
 		setNamespacedAttribute(request, "ddmFormValues", _ddmFormValues);
-		setNamespacedAttribute(request, "ddmFormValuesNamespace",
-			_ddmFormValuesNamespace);
+		setNamespacedAttribute(request, "ddmFormValuesNamespace", _ddmFormValuesNamespace);
 		setNamespacedAttribute(request, "readOnly", _readOnly);
 		setNamespacedAttribute(request, "repeatable", _repeatable);
 		setNamespacedAttribute(request, "requestedLocale", _requestedLocale);
-		setNamespacedAttribute(request, "showEmptyFieldLabel",
-			_showEmptyFieldLabel);
+		setNamespacedAttribute(request, "showEmptyFieldLabel", _showEmptyFieldLabel);
 	}
 
 	protected static final String _ATTRIBUTE_NAMESPACE = "ddm:html:";
