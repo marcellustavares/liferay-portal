2cb2ce39c6ce58e7be7c2e0e28ed2438618e9138
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-02-26 17:25:26 -0300

    js rendering

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-text/src/META-INF/resources/text.soy.js
@@ -0,0 +1,9 @@
+// This file was automatically generated from text.soy.
+// Please don't edit this file by hand.
+
+if (typeof ddm == 'undefined') { var ddm = {}; }
+
+
+ddm.text = function(opt_data, opt_ignored) {
+  return '\t<div class="form-group field-wrapper" data-fieldname="' + soy.$$escapeHtml(opt_data.fieldName) + '" data-fieldnamespace="' + soy.$$escapeHtml(opt_data.fieldNameSuffix) + '"><label class="control-fieldLabel" for="' + soy.$$escapeHtml(opt_data.fieldQualifiedName) + '">' + soy.$$escapeHtml(opt_data.fieldLabel) + '</label><input class="field form-control" dir="' + soy.$$escapeHtml(opt_data.dir) + '" id="' + soy.$$escapeHtml(opt_data.fieldQualifiedName) + '" name="' + soy.$$escapeHtml(opt_data.fieldQualifiedName) + '" type="text" value="' + soy.$$escapeHtml(opt_data.fieldValue) + '"></div>';
+};
