968d795d0c67b937aecf565af6321076f7e33a46
Bruno Basto <bruno.basto@liferay.com>
2015-05-30 17:19:02 -0300

    [DDM Renderer] Refactoring field.js

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/META-INF/resources/js/field.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/META-INF/resources/js/field.js
@@ -2,9 +2,7 @@ AUI.add(
 	'liferay-ddm-form-renderer-field',
 	function(A) {
 		var AArray = A.Array;
-		var FieldTypes = Liferay.DDM.Renderer.FieldTypes;
 		var Lang = A.Lang;
-		var Util = Liferay.DDM.Renderer.Util;
 
 		var SELECTOR_REPEAT_BUTTONS = '.lfr-ddm-form-field-repeatable-add-button, .lfr-ddm-form-field-repeatable-delete-button';
 
@@ -20,19 +18,23 @@ AUI.add(
 						valueFn: '_valueContainer'
 					},
 
-					fieldType: {
-						getter: '_getFieldType'
-					},
-
 					definition: {
 						value: {}
+					},
+
+					fields: {
+						value: []
+					},
+
+					fieldType: {
+						value: ''
 					},
 
 					form: {
 					},
 
 					instanceId: {
-						value: ''
+						valueFn: '_valueInstanceId'
 					},
 
 					label: {
@@ -43,8 +45,16 @@ AUI.add(
 						value: themeDisplay.getLanguageId()
 					},
 
+					localizable: {
+						getter: '_getLocalizable'
+					},
+
 					name: {
 						getter: '_getName'
+					},
+
+					pages: {
+						value: undefined
 					},
 
 					parent: {
@@ -62,16 +72,30 @@ AUI.add(
 
 					repeatedIndex: {
 						valueFn: '_valueRepeatedIndex'
+					},
+
+					value: {
+						value: ''
 					}
 				},
 
-				EXTENDS: A.FormField,
+				EXTENDS: A.Base,
 
 				NAME: 'liferay-ddm-form-renderer-field',
 
 				prototype: {
 					renderUI: function() {
 						var instance = this;
+
+						var container = instance.get('container');
+
+						if (!container.inDoc()) {
+							instance.renderTemplate();
+
+							var parent = instance.get('parent');
+
+							container.appendTo(parent.get('container'));
+						}
 
 						if (instance.get('repeatable')) {
 							instance.renderRepeatableUI();
@@ -98,15 +122,9 @@ AUI.add(
 					destructor: function() {
 						var instance = this;
 
+						instance.get('container').remove();
+
 						(new A.EventHandle(instance._eventHandlers)).detach();
-					},
-
-					getIndex: function() {
-						var instance = this;
-
-						var form = instance.get('form');
-
-						return form.getFieldNodes().indexOf(instance.get('container'));
 					},
 
 					getInputNode: function() {
@@ -158,7 +176,13 @@ AUI.add(
 					getTemplate: function() {
 						var instance = this;
 
-						return FieldTypes.getFieldTypeTemplate(
+						var value = instance.get('value');
+
+						if (instance.get('localizable')) {
+							value = value[instance.get('locale')];
+						}
+
+						return Liferay.DDM.Renderer.FieldTypes.getFieldTypeTemplate(
 							instance.get('fieldType'),
 							A.merge(
 								instance.get('definition'),
@@ -167,7 +191,7 @@ AUI.add(
 									label: instance.get('label'),
 									name: instance.getQualifiedName(),
 									placeholder: '',
-									value: ''
+									value: value || ''
 								}
 							)
 						);
@@ -190,8 +214,6 @@ AUI.add(
 
 						siblings.splice(index, 1);
 
-						instance.destroy();
-
 						instance.get('container').remove(true);
 
 						instance.fire(
@@ -200,18 +222,18 @@ AUI.add(
 								field: instance
 							}
 						);
+
+						instance.destroy();
 					},
 
 					render: function() {
 						var instance = this;
 
-						if (!instance.get('container').inDoc()) {
-							instance.renderTemplate();
-						}
-
 						instance.renderUI();
 						instance.bindUI();
 						instance.syncUI();
+
+						return instance;
 					},
 
 					renderRepeatableUI: function() {
@@ -226,9 +248,13 @@ AUI.add(
 					renderTemplate: function() {
 						var instance = this;
 
+						var container = instance.get('container');
+
+						var parent = instance.get('parent');
+
 						var template = instance.getTemplate();
 
-						instance.get('container').html(template);
+						container.html(template);
 					},
 
 					repeat: function() {
@@ -236,20 +262,17 @@ AUI.add(
 
 						var fieldType = instance.get('fieldType');
 
-						var FieldClass = Util.getFieldClass(fieldType);
-
 						var siblings = instance.getSiblings();
 
 						var index = AArray.indexOf(siblings, instance);
 
 						var form = instance.get('form');
 
-						var field = new FieldClass(
+						var field = new Field(
 							{
 								definition: instance.get('definition'),
 								fieldType: fieldType,
 								form: form,
-								instanceId: Util.generateInstanceId(8),
 								parent: instance.get('parent'),
 								portletNamespace: instance.get('portletNamespace'),
 								repeatedIndex: index + 1
@@ -284,16 +307,18 @@ AUI.add(
 						container.one('.lfr-ddm-form-field-repeatable-delete-button').toggle(instance.get('repeatedIndex') > 0);
 					},
 
-					_getFieldType: function() {
-						var instance = this;
-
-						return instance.get('definition').type;
-					},
-
 					_getLabel: function() {
 						var instance = this;
 
-						return instance.get('definition').label[instance.get('locale')];
+						var definition = instance.get('definition');
+
+						return definition.label[instance.get('locale')] || definition.label;
+					},
+
+					_getLocalizable: function() {
+						var instance = this;
+
+						return !!instance.get('definition').localizable;
 					},
 
 					_getName: function() {
@@ -343,6 +368,12 @@ AUI.add(
 						return A.Node.create('<div class="lfr-ddm-form-field-container"></div>');
 					},
 
+					_valueInstanceId: function() {
+						var instance = this;
+
+						return Liferay.DDM.Renderer.Util.generateInstanceId(8);
+					},
+
 					_valueRepeatedIndex: function() {
 						var instance = this;
 
@@ -350,29 +381,31 @@ AUI.add(
 
 						var repeatedIndex = -1;
 
-						var found = false;
+						if (A.instanceOf(form, Liferay.DDM.Renderer.Form)) {
+							var found = false;
 
-						form.getFieldNodes().each(
-							function(item) {
-								if (!found) {
-									var fieldNode = item.one('.field-wrapper');
+							form.getFieldNodes().each(
+								function(item) {
+									if (!found) {
+										var fieldNode = item.one('.field-wrapper');
 
-									var fieldQualifiedName = fieldNode.getData('fieldname');
+										var fieldQualifiedName = fieldNode.getData('fieldname');
 
-									var fieldInstanceId = Util.getInstanceIdFromQualifiedName(fieldQualifiedName);
+										var fieldInstanceId = Liferay.DDM.Renderer.Util.getInstanceIdFromQualifiedName(fieldQualifiedName);
 
-									if (fieldInstanceId === instance.get('instanceId')) {
-										found = true;
-									}
+										if (fieldInstanceId === instance.get('instanceId')) {
+											found = true;
+										}
 
-									var fieldName = Util.getFieldNameFromQualifiedName(fieldQualifiedName);
+										var fieldName = Liferay.DDM.Renderer.Util.getFieldNameFromQualifiedName(fieldQualifiedName);
 
-									if (fieldName === instance.get('name')) {
-										repeatedIndex++;
+										if (fieldName === instance.get('name')) {
+											repeatedIndex++;
+										}
 									}
 								}
-							}
-						);
+							);
+						}
 
 						return repeatedIndex;
 					}
@@ -384,6 +417,6 @@ AUI.add(
 	},
 	'',
 	{
-		requires: ['aui-form-field', 'liferay-ddm-form-renderer-field-types']
+		requires: ['aui-form-builder-field-base', 'aui-form-field', 'liferay-ddm-form-renderer-field-types']
 	}
 );
