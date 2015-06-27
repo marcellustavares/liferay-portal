92c4cc01c3f610af064175f3d0566e8b8069792f
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-06-27 13:01:42 -0300

    LPS-56454 SF

--- modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/com/liferay/dynamic/data/lists/upgrade/v1_0_0/DDLRecordVersionsUpgradeProcess.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/com/liferay/dynamic/data/lists/upgrade/v1_0_0/DDLRecordVersionsUpgradeProcess.java
@@ -47,29 +47,6 @@ import java.util.Set;
  */
 public class DDLRecordVersionsUpgradeProcess extends UpgradeProcess {
 
-	public String toXML(Map<String, String> expandoValuesMap) {
-		Document document = SAXReaderUtil.createDocument();
-
-		Element rootElement = document.addElement("root");
-
-		for (Map.Entry<String, String> entry : expandoValuesMap.entrySet()) {
-			Element dynamicElementElement = rootElement.addElement(
-				"dynamic-element");
-
-			String name = entry.getKey();
-			String data = entry.getValue();
-
-			dynamicElementElement.addAttribute("name", name);
-			dynamicElementElement.addAttribute(
-				"default-language-id",
-				LocalizationUtil.getDefaultLanguageId(data));
-
-			addDynamicContentElements(dynamicElementElement, name, data);
-		}
-
-		return document.asXML();
-	}
-
 	protected void addDDMContent(
 			String uuid_, long contentId, long groupId, long companyId,
 			long userId, String userName, Timestamp createDate,
@@ -254,12 +231,45 @@ public class DDLRecordVersionsUpgradeProcess extends UpgradeProcess {
 		return sb.toString();
 	}
 
+	protected boolean isUpgradeActive() throws Exception {
+		if (hasTable("DDLRecord") && hasTable("DDLRecordSet") &&
+			hasTable("DDLRecordVersion")) {
+
+			return true;
+		}
+
+		return false;
+	}
+
 	protected void setUpClassNameIds() {
 		_ddmContentClassNameId = PortalUtil.getClassNameId(DDMContent.class);
 
 		_expandoStorageAdapterClassNameId = PortalUtil.getClassNameId(
 			"com.liferay.portlet.dynamicdatamapping.storage." +
 				"ExpandoStorageAdapter");
+	}
+
+	protected String toXML(Map<String, String> expandoValuesMap) {
+		Document document = SAXReaderUtil.createDocument();
+
+		Element rootElement = document.addElement("root");
+
+		for (Map.Entry<String, String> entry : expandoValuesMap.entrySet()) {
+			Element dynamicElementElement = rootElement.addElement(
+				"dynamic-element");
+
+			String name = entry.getKey();
+			String data = entry.getValue();
+
+			dynamicElementElement.addAttribute("name", name);
+			dynamicElementElement.addAttribute(
+				"default-language-id",
+				LocalizationUtil.getDefaultLanguageId(data));
+
+			addDynamicContentElements(dynamicElementElement, name, data);
+		}
+
+		return document.asXML();
 	}
 
 	protected void updateDDMStorageLink(
