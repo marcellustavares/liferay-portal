7e5ece08508ec7f8110bd476989f51ace4daac58
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-08-01 10:41:41 -0300

    LPS-57255 Fix initialization issues

--- modules/apps/document-library/document-library-google-docs/src/com/liferay/document/library/google/docs/configuration/configurator/GoogleDocsConfigurator.java
+++ modules/apps/document-library/document-library-google-docs/src/com/liferay/document/library/google/docs/configuration/configurator/GoogleDocsConfigurator.java
@@ -27,6 +27,7 @@ import com.liferay.portal.service.UserLocalService;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalService;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryMetadataLocalService;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryTypeLocalService;
+import com.liferay.portlet.dynamicdatamapping.DDMStructureLinkManager;
 
 import javax.servlet.ServletContext;
 
@@ -86,59 +87,64 @@ public class GoogleDocsConfigurator {
 	}
 
 	@Reference
-	public void setClassNameLocalService(
+	protected void setClassNameLocalService(
 		ClassNameLocalService classNameLocalService) {
 
 		_classNameLocalService = classNameLocalService;
 	}
 
 	@Reference
-	public void setCompanyLocalService(
+	protected void setCompanyLocalService(
 		CompanyLocalService companyLocalService) {
 
 		_companyLocalService = companyLocalService;
 	}
 
 	@Reference
-	public void setDDMStructureLocalService(
+	protected void setDDMStructureLinkManager(
+		DDMStructureLinkManager ddmStructureLinkManager) {
+	}
+
+	@Reference
+	protected void setDDMStructureLocalService(
 		DDMStructureLocalService ddmStructureLocalService) {
 
 		_ddmStructureLocalService = ddmStructureLocalService;
 	}
 
 	@Reference
-	public void setDLFileEntryLocalService(
+	protected void setDLFileEntryLocalService(
 		DLFileEntryLocalService dlFileEntryLocalService) {
 
 		_dlFileEntryLocalService = dlFileEntryLocalService;
 	}
 
 	@Reference
-	public void setDLFileEntryMetadataLocalService(
+	protected void setDLFileEntryMetadataLocalService(
 		DLFileEntryMetadataLocalService dlFileEntryMetadataLocalService) {
 
 		_dlFileEntryMetadataLocalService = dlFileEntryMetadataLocalService;
 	}
 
 	@Reference
-	public void setDLFileEntryTypeLocalService(
+	protected void setDLFileEntryTypeLocalService(
 		DLFileEntryTypeLocalService dlFileEntryTypeLocalService) {
 
 		_dlFileEntryTypeLocalService = dlFileEntryTypeLocalService;
 	}
 
+	@Reference(target = "(original.bean=true)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
+	}
+
 	@Reference
-	public void setStorageEngine(StorageEngine storageEngine) {
+	protected void setStorageEngine(StorageEngine storageEngine) {
 		_storageEngine = storageEngine;
 	}
 
 	@Reference
-	public void setUserLocalService(UserLocalService userLocalService) {
+	protected void setUserLocalService(UserLocalService userLocalService) {
 		_userLocalService = userLocalService;
-	}
-
-	@Reference(target = "(original.bean=true)", unbind = "-")
-	protected void setServletContext(ServletContext servletContext) {
 	}
 
 	private ClassNameLocalService _classNameLocalService;
