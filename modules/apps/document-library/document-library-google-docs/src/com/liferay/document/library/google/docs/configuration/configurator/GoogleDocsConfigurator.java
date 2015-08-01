88cbf5d5c3d43971243fb32565ba59d56739dd2e
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-08-01 10:41:41 -0300

    LPS-57255 Fix initialization issues

--- modules/apps/document-library/document-library-google-docs/src/com/liferay/document/library/google/docs/configuration/configurator/GoogleDocsConfigurator.java
+++ modules/apps/document-library/document-library-google-docs/src/com/liferay/document/library/google/docs/configuration/configurator/GoogleDocsConfigurator.java
@@ -20,7 +20,6 @@ import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.dynamic.data.mapping.storage.StorageEngine;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.service.ClassNameLocalService;
 import com.liferay.portal.service.CompanyLocalService;
@@ -28,6 +27,9 @@ import com.liferay.portal.service.UserLocalService;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalService;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryMetadataLocalService;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryTypeLocalService;
+import com.liferay.portlet.dynamicdatamapping.DDMStructureLinkManager;
+
+import javax.servlet.ServletContext;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -85,58 +87,63 @@ public class GoogleDocsConfigurator {
 	}
 
 	@Reference
-	public void setClassNameLocalService(
+	protected void setClassNameLocalService(
 		ClassNameLocalService classNameLocalService) {
 
 		_classNameLocalService = classNameLocalService;
 	}
 
 	@Reference
-	public void setCompanyLocalService(
+	protected void setCompanyLocalService(
 		CompanyLocalService companyLocalService) {
 
 		_companyLocalService = companyLocalService;
 	}
 
 	@Reference
-	public void setDDMStructureLocalService(
+	protected void setDDMStructureLinkManager(
+		DDMStructureLinkManager ddmStructureLinkManager) {
+	}
+
+	@Reference
+	protected void setDDMStructureLocalService(
 		DDMStructureLocalService ddmStructureLocalService) {
 
 		_ddmStructureLocalService = ddmStructureLocalService;
 	}
 
 	@Reference
-	public void setDLFileEntryLocalService(
+	protected void setDLFileEntryLocalService(
 		DLFileEntryLocalService dlFileEntryLocalService) {
 
 		_dlFileEntryLocalService = dlFileEntryLocalService;
 	}
 
 	@Reference
-	public void setDLFileEntryMetadataLocalService(
+	protected void setDLFileEntryMetadataLocalService(
 		DLFileEntryMetadataLocalService dlFileEntryMetadataLocalService) {
 
 		_dlFileEntryMetadataLocalService = dlFileEntryMetadataLocalService;
 	}
 
 	@Reference
-	public void setDLFileEntryTypeLocalService(
+	protected void setDLFileEntryTypeLocalService(
 		DLFileEntryTypeLocalService dlFileEntryTypeLocalService) {
 
 		_dlFileEntryTypeLocalService = dlFileEntryTypeLocalService;
 	}
 
 	@Reference
-	public void setStorageEngine(StorageEngine storageEngine) {
+	protected void setStorageEngine(StorageEngine storageEngine) {
 		_storageEngine = storageEngine;
 	}
 
 	@Reference
-	public void setUserLocalService(UserLocalService userLocalService) {
+	protected void setUserLocalService(UserLocalService userLocalService) {
 		_userLocalService = userLocalService;
 	}
 
-	@Reference(
+    @Reference(
 		target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-"
 	)
 	protected void setModuleServiceLifecycle(
