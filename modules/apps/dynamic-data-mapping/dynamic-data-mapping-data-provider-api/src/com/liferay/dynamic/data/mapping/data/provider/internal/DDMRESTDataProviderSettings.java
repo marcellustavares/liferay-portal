d7ba08f1cf3bfb0b9bc1c14795dd423a307b1573
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-09-03 19:35:17 -0300

    LPS-XXX DDM Data Provider

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-api/src/com/liferay/dynamic/data/mapping/data/provider/internal/DDMRESTDataProviderSettings.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.data.provider.internal;
+
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderSettings;
+import com.liferay.dynamic.data.mapping.registry.annotations.DDMForm;
+import com.liferay.dynamic.data.mapping.registry.annotations.DDMFormField;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marcellus Tavares
+ */
+@Component(immediate = true, properties = "ddm.data.provider.name=rest")
+public class DDMRESTDataProviderSettings implements DDMDataProviderSettings {
+
+	@Override
+	public Class<?> getSettings() {
+		return RESTSettings.class;
+	}
+
+	@DDMForm
+	public interface RESTSettings {
+
+		public String key();
+
+		public String password();
+
+		@DDMFormField
+		public String url();
+
+		public String username();
+
+		public String value();
+
+	}
+
+}
