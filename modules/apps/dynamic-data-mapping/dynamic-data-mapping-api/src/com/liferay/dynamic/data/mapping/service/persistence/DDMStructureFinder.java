b455d35aaad5372e010d27bff862c47ef5221689
Marcellus Tavares <marcellus.tavares@liferay.com>
2015-09-09 12:42:49 -0300

    LPS-56688 Regenerate

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/com/liferay/dynamic/data/mapping/service/persistence/DDMStructureFinder.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/com/liferay/dynamic/data/mapping/service/persistence/DDMStructureFinder.java
@@ -27,66 +27,60 @@ public interface DDMStructureFinder {
 
 	public int countByC_G_C_N_D_S_T_S(long companyId, long[] groupIds,
 		long classNameId, java.lang.String name, java.lang.String description,
-		java.lang.String storageType, int type, boolean andOperator, int status);
+		java.lang.String storageType, int type, int status, boolean andOperator);
 
 	public int countByC_G_C_N_D_S_T_S(long companyId, long[] groupIds,
 		long classNameId, java.lang.String[] names,
 		java.lang.String[] descriptions, java.lang.String storageType,
-		int type, boolean andOperator, int status);
+		int type, int status, boolean andOperator);
 
 	public int filterCountByKeywords(long companyId, long[] groupIds,
 		long classNameId, java.lang.String keywords, int status);
 
 	public int filterCountByC_G_C_N_D_S_T_S(long companyId, long[] groupIds,
 		long classNameId, java.lang.String name, java.lang.String description,
-		java.lang.String storageType, int type, boolean andOperator, int status);
+		java.lang.String storageType, int type, int status, boolean andOperator);
 
 	public int filterCountByC_G_C_N_D_S_T_S(long companyId, long[] groupIds,
 		long classNameId, java.lang.String[] names,
 		java.lang.String[] descriptions, java.lang.String storageType,
-		int type, boolean andOperator, int status);
+		int type, int status, boolean andOperator);
 
 	public java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructure> filterFindByKeywords(
 		long companyId, long[] groupIds, long classNameId,
-		java.lang.String keywords, int start, int end,
-		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator,
-		int status);
+		java.lang.String keywords, int status, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator);
 
 	public java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructure> filterFindByC_G_C_N_D_S_T_S(
 		long companyId, long[] groupIds, long classNameId,
 		java.lang.String name, java.lang.String description,
-		java.lang.String storageType, int type, boolean andOperator, int start,
-		int end,
-		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator,
-		int status);
+		java.lang.String storageType, int type, int status,
+		boolean andOperator, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator);
 
 	public java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructure> filterFindByC_G_C_N_D_S_T_S(
 		long companyId, long[] groupIds, long classNameId,
 		java.lang.String[] names, java.lang.String[] descriptions,
-		java.lang.String storageType, int type, boolean andOperator, int start,
-		int end,
-		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator,
-		int status);
+		java.lang.String storageType, int type, int status,
+		boolean andOperator, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator);
 
 	public java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructure> findByKeywords(
 		long companyId, long[] groupIds, long classNameId,
-		java.lang.String keywords, int start, int end,
-		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator,
-		int status);
+		java.lang.String keywords, int status, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator);
 
 	public java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructure> findByC_G_C_N_D_S_T_S(
 		long companyId, long[] groupIds, long classNameId,
 		java.lang.String name, java.lang.String description,
-		java.lang.String storageType, int type, boolean andOperator, int start,
-		int end,
-		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator,
-		int status);
+		java.lang.String storageType, int type, int status,
+		boolean andOperator, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator);
 
 	public java.util.List<com.liferay.dynamic.data.mapping.model.DDMStructure> findByC_G_C_N_D_S_T_S(
 		long companyId, long[] groupIds, long classNameId,
 		java.lang.String[] names, java.lang.String[] descriptions,
-		java.lang.String storageType, int type, boolean andOperator, int start,
-		int end,
-		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator,
-		int status);
+		java.lang.String storageType, int type, int status,
+		boolean andOperator, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator<com.liferay.dynamic.data.mapping.model.DDMStructure> orderByComparator);
 }
