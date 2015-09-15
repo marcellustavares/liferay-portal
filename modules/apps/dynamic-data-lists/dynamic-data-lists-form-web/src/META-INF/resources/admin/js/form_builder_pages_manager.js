cb45d4d1a27e317291c9ea5717f15b4f5e516e91
Fernando Souza <fernando.souza@liferay.com>
2015-09-15 18:27:29 -0300

    LPS-58425 Adds page style and second dropdown with controls options.

--- modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/META-INF/resources/admin/js/form_builder_pages_manager.js
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/META-INF/resources/admin/js/form_builder_pages_manager.js
@@ -5,6 +5,14 @@ AUI.add(
 		var Renderer = Liferay.DDM.Renderer;
 
 		var CSS_FORM_BUILDER_TABVIEW = A.getClassName('form', 'builder', 'tabview');
+		var CSS_FORM_BUILDER_PAGE_MANAGER_POPOVER = A.getClassName('form', 'builder', 'page', 'manager', 'popover');
+		var CSS_FORM_BUILDER_PAGE_MANAGER_ADD_PAGE_LAST_POSITION = A.getClassName('form', 'builder', 'page', 'manager', 'add', 'last', 'position');
+		var CSS_FORM_BUILDER_PAGE_MANAGER_DELETE_PAGE = A.getClassName('form', 'builder', 'page', 'manager', 'delete', 'page');
+		var CSS_FORM_BUILDER_PAGE_MANAGER_SWITCH_MODE = A.getClassName('form', 'builder', 'page', 'manager', 'switch', 'mode');
+		var CSS_FORM_BUILDER_PAGES_CONTENT = A.getClassName('form', 'builder', 'page', 'manager', 'content');
+		var CSS_FORM_BUILDER_PAGINATION = A.getClassName('form', 'builder', 'pagination');
+		var CSS_FORM_BUILDER_PAGE_CONTROLS = A.getClassName('form', 'builder', 'page', 'controls');
+		var CSS_FORM_BUILDER_SWITCH_VIEW = A.getClassName('form', 'builder', 'switch', 'view');
 
 		var FormBuilderPagesManager = A.Component.create(
 			{
@@ -22,6 +30,65 @@ AUI.add(
 				EXTENDS: A.FormBuilderPageManager,
 
 				prototype: {
+					TPL_PAGES: '<div class="' + CSS_FORM_BUILDER_PAGES_CONTENT + '">' +
+						'<div class="' + CSS_FORM_BUILDER_PAGINATION + '">' +
+							'<div class="' + CSS_FORM_BUILDER_PAGE_CONTROLS + '">' +
+								'<a href="javascript:;" class="' + CSS_FORM_BUILDER_SWITCH_VIEW + ' glyphicon glyphicon-th"></a>' +
+							'</div>'+
+						'</div></div>',
+
+					TPL_HEADER_PAGE_CONTROL: 
+						'<div class="liferay-ddl-form-builder-header-controls">' +
+							'<div id="formBuilderHeaderControls" class="dropdown">' +
+								'<a href="javascript:;" class="dropdown-toggle form-builder-header-controls-trigger">' +
+									'<span class="icon-ellipsis-vertical icon-monospaced"></span>' +
+								'</a>' +
+							'</div>' +
+						'</div>',
+					
+					initializer: function() {
+						A.one('.' + CSS_FORM_BUILDER_TABVIEW).append(this.TPL_HEADER_PAGE_CONTROL);
+
+						this._createHeaderPopover();
+					},
+
+					_createHeaderPopover: function() {
+						var trigger = A.one('.form-builder-header-controls-trigger');
+
+						var options = A.Lang.sub(this.TPL_POPOVER_CONTENT, {
+							addPageLastPosition: this.get('strings').addPageLastPosition,
+							addPageNextPosition: this.get('strings').addPageNextPosition,
+							deleteCurrentPage: this.get('strings').deleteCurrentPage,
+							switchMode: this.get('strings').switchMode
+						});
+
+						var popover = new A.Popover({
+								align: {
+									node: trigger,
+									 points:[A.WidgetPositionAlign.RC, A.WidgetPositionAlign.LC]
+								},
+								bodyContent: options,
+								constrain: true,
+								cssClass: 'form-builder-page-manager-popover-header',
+								visible: false,
+								position: 'left',
+								zIndex: 50
+						  }).render();
+
+						trigger.after('click', popover.toggle, popover);
+						trigger.after('clickoutside', popover.hide, popover);
+
+						popover.get('boundingBox').one('.' + CSS_FORM_BUILDER_PAGE_MANAGER_ADD_PAGE_LAST_POSITION).on('click',
+							A.bind(this._onAddLastPageClick, this)
+						);
+						popover.get('boundingBox').one('.' + CSS_FORM_BUILDER_PAGE_MANAGER_DELETE_PAGE).on('click',
+							A.bind(this._onRemovePageClick, this)
+						);
+						popover.get('boundingBox').one('.' + CSS_FORM_BUILDER_PAGE_MANAGER_SWITCH_MODE).on('click',
+							A.bind(this._onSwitchViewClick, this)
+						);
+					},
+
 					_addWizardPage: function() {
 						var instance = this;
 
@@ -86,9 +153,9 @@ AUI.add(
 					_getWizardView: function() {
 						var instance = this;
 
-						var wizardNode = A.one('.' + CSS_FORM_BUILDER_TABVIEW);
-
 						if (!instance.wizard) {
+							var wizardNode = A.one('.' + CSS_FORM_BUILDER_TABVIEW);
+
 							instance.wizard = new Renderer.Wizard(
 								{
 									allowNavigation: true,
@@ -110,6 +177,16 @@ AUI.add(
 						instance._addPage();
 
 						instance._addWizardPage();
+					},
+
+					_onAddLastPageClick: function() {
+						var instance = this;
+
+						instance._addPage();
+
+						instance._addWizardPage();
+						
+						instance._getPopover().hide();
 					},
 
 					_onRemovePageClick: function() {
@@ -210,16 +287,16 @@ AUI.add(
 						var wizardView = instance._getWizardView();
 
 						if (type === 'wizard') {
-							pagination.get('contentBox').hide();
+							pagination.get('boundingBox').hide();
 
-							wizardView.get('contentBox').show();
+							wizardView.get('boundingBox').show();
 
 							instance._updateWizardContent();
 						}
 						else if (type === 'pagination') {
-							pagination.get('contentBox').show();
+							pagination.get('boundingBox').show();
 
-							wizardView.get('contentBox').hide();
+							wizardView.get('boundingBox').hide();
 
 							pagination.set('page', activePageNumber);
 						}
