cca46623e288183c2dfe1de688c5e7fea86d85ca
Leonardo Barros <leonardo.barros@liferay.com>
2015-11-07 11:22:10 -0300

    LPS-60170 Implement add functionality for data provider

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-web/src/main/java/com/liferay/dynamic/data/mapping/data/provider/web/portlet/DDMDataProviderPortlet.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-data-provider-web/src/main/java/com/liferay/dynamic/data/mapping/data/provider/web/portlet/DDMDataProviderPortlet.java
@@ -14,9 +14,16 @@
 
 package com.liferay.dynamic.data.mapping.data.provider.web.portlet;
 
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderSettings;
 import com.liferay.dynamic.data.mapping.data.provider.web.constants.DDMDataProviderPortletKeys;
 import com.liferay.dynamic.data.mapping.data.provider.web.display.context.DDMDataProviderDisplayContext;
+import com.liferay.dynamic.data.mapping.form.renderer.DDMFormRenderer;
+import com.liferay.dynamic.data.mapping.service.DDMDataProviderLocalService;
 import com.liferay.dynamic.data.mapping.service.DDMDataProviderService;
+import com.liferay.osgi.service.tracker.map.ServiceTrackerCustomizerFactory;
+import com.liferay.osgi.service.tracker.map.ServiceTrackerCustomizerFactory.ServiceWrapper;
+import com.liferay.osgi.service.tracker.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.map.ServiceTrackerMapFactory;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -27,7 +34,11 @@ import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
@@ -67,12 +78,38 @@ public class DDMDataProviderPortlet extends MVCPortlet {
 
 		DDMDataProviderDisplayContext ddmDataProviderDisplayContext =
 			new DDMDataProviderDisplayContext(
-				renderRequest, renderResponse, _ddmDataProviderService);
+				renderRequest, renderResponse, _ddmDataProviderService,
+				_ddmDataProviderLocalService, _ddmFormRenderer,
+				_ddmDataProvidersMap);
 
 		renderRequest.setAttribute(
 			WebKeys.PORTLET_DISPLAY_CONTEXT, ddmDataProviderDisplayContext);
 
 		super.render(renderRequest, renderResponse);
+	}
+
+	@Activate
+	protected void activate(final BundleContext bundleContext)
+		throws InvalidSyntaxException {
+
+		_ddmDataProvidersMap = ServiceTrackerMapFactory.singleValueMap(
+			bundleContext, DDMDataProviderSettings.class,
+			"ddm.data.provider.name",
+			ServiceTrackerCustomizerFactory.<DDMDataProviderSettings>
+				serviceWrapper(bundleContext));
+		_ddmDataProvidersMap.open();
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_ddmDataProvidersMap.close();
+	}
+
+	@Reference(unbind = "-")
+	protected void setDDMDataProviderLocalService(
+		DDMDataProviderLocalService ddmDataProviderLocalService) {
+
+		_ddmDataProviderLocalService = ddmDataProviderLocalService;
 	}
 
 	@Reference(unbind = "-")
@@ -82,6 +119,15 @@ public class DDMDataProviderPortlet extends MVCPortlet {
 		_ddmDataProviderService = ddmDataProviderService;
 	}
 
+	@Reference(unbind = "-")
+	protected void setDDMFormRenderer(DDMFormRenderer ddmFormRenderer) {
+		_ddmFormRenderer = ddmFormRenderer;
+	}
+
+	private DDMDataProviderLocalService _ddmDataProviderLocalService;
 	private DDMDataProviderService _ddmDataProviderService;
+	private ServiceTrackerMap<String, ServiceWrapper<DDMDataProviderSettings>>
+		_ddmDataProvidersMap;
+	private DDMFormRenderer _ddmFormRenderer;
 
 }
