8a3c1c158be2c9fbd01887a97c79ad477dfda2ad
Leonardo Barros <leonardo.barros@liferay.com>
2015-12-01 10:25:50 -0300

    LPS-60860 Add the default layout only if it does not exist

--- modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/events/AddDefaultSharedPortletLayoutAction.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/events/AddDefaultSharedPortletLayoutAction.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.model.Company;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.GroupConstants;
+import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutConstants;
 import com.liferay.portal.security.auth.CompanyThreadLocal;
 import com.liferay.portal.service.CompanyLocalService;
@@ -71,27 +72,32 @@ public class AddDefaultSharedPortletLayoutAction extends SimpleAction {
 	}
 
 	protected void doRun(long companyId) throws Exception {
-		ServiceContext serviceContext = new ServiceContext();
-
-		serviceContext.setAddGuestPermissions(true);
-		serviceContext.setAddGroupPermissions(true);
-		serviceContext.setAttribute(
-			"layout.instanceable.allowed", Boolean.TRUE);
-
 		Group group = _groupLocalService.getGroup(
 			companyId, GroupConstants.GUEST);
 
-		serviceContext.setScopeGroupId(group.getGroupId());
+		Layout layout = _layoutLocalService.fetchLayoutByFriendlyURL(
+			group.getGroupId(), false, "/shared");
 
-		long defaultUserId = _userLocalService.getDefaultUserId(companyId);
+		if (layout == null) {
+			ServiceContext serviceContext = new ServiceContext();
 
-		serviceContext.setUserId(defaultUserId);
+			serviceContext.setAddGuestPermissions(true);
+			serviceContext.setAddGroupPermissions(true);
+			serviceContext.setAttribute(
+				"layout.instanceable.allowed", Boolean.TRUE);
 
-		_layoutLocalService.addLayout(
-			defaultUserId, group.getGroupId(), false,
-			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "shared",
-			StringPool.BLANK, StringPool.BLANK, "shared_portlet", true,
-			"/shared", serviceContext);
+			serviceContext.setScopeGroupId(group.getGroupId());
+
+			long defaultUserId = _userLocalService.getDefaultUserId(companyId);
+
+			serviceContext.setUserId(defaultUserId);
+
+			_layoutLocalService.addLayout(
+				defaultUserId, group.getGroupId(), false,
+				LayoutConstants.DEFAULT_PARENT_LAYOUT_ID, "shared",
+				StringPool.BLANK, StringPool.BLANK, "shared_portlet", true,
+				"/shared", serviceContext);
+		}
 	}
 
 	@Reference(unbind = "-")
