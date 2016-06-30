599e033a74bdcfa96f4664dd6d6ef4d4cd0c448d
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-06-30 18:33:30 -0300

    WIP

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-expression/src/main/java/com/liferay/dynamic/data/mapping/expression/internal/DDMExpressionListenerImpl.java
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-expression/src/main/java/com/liferay/dynamic/data/mapping/expression/internal/DDMExpressionListenerImpl.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p/>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p/>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -16,20 +16,21 @@ package com.liferay.dynamic.data.mapping.expression.internal;
 
 import com.liferay.dynamic.data.mapping.expression.internal.parser.DDMExpressionBaseListener;
 import com.liferay.dynamic.data.mapping.expression.internal.parser.DDMExpressionParser;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import org.antlr.v4.runtime.misc.NotNull;
 
 /**
  * @author Marcellus Tavares
  */
 public class DDMExpressionListenerImpl extends DDMExpressionBaseListener {
 
-	public void enterNumericVariable(
-		@NotNull DDMExpressionParser.NumericVariableContext ctx) {
+	public void enterFunctionCall(
+		@NotNull DDMExpressionParser.FunctionCallContext ctx) {
 
-		_variableNames.add(ctx.getText());
+		_functionNames.add(ctx.functionName.getText());
 	}
 
 	public void enterLogicalVariable(
@@ -38,22 +39,21 @@ public class DDMExpressionListenerImpl extends DDMExpressionBaseListener {
 		_variableNames.add(ctx.getText());
 	}
 
-	public void enterFunctionCall(
-		@NotNull DDMExpressionParser.FunctionCallContext ctx) {
+	public void enterNumericVariable(
+		@NotNull DDMExpressionParser.NumericVariableContext ctx) {
 
-		_functionNames.add(ctx.functionName.getText());
-	}
-
-	public Set<String> getVariableNames() {
-		return _variableNames;
+		_variableNames.add(ctx.getText());
 	}
 
 	public Set<String> getFunctionNames() {
 		return _functionNames;
 	}
 
-	private Set<String> _variableNames = new HashSet<>();
-	private Set<String> _functionNames = new HashSet<>();
+	public Set<String> getVariableNames() {
+		return _variableNames;
+	}
 
+	private final Set<String> _functionNames = new HashSet<>();
+	private final Set<String> _variableNames = new HashSet<>();
 
 }
