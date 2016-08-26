13d279ea2c7cd2ed1586779e13db495c9712ee58
Adam Victor Nazareth Brandizzi <adam.brandizzi@liferay.com>
2016-08-26 15:51:45 -0300

    LPS-67663 Override JavaScript modules: rename config, remove non-updated modules, turn updated ones into overrides.

--- modules/apps/events-calendar/events-calendar-web/src/main/resources/META-INF/resources/js/config.js
+++ modules/apps/events-calendar/events-calendar-web/src/main/resources/META-INF/resources/js/config.js
@@ -2,7 +2,7 @@
 	AUI().applyConfig(
 		{
 			groups: {
-				calendar: {
+				'events-calendar': {
 					base: MODULE_PATH + '/js/',
 					combine: Liferay.AUI.getCombine(),
 					filter: Liferay.AUI.getFilterConfig(),
@@ -16,58 +16,6 @@
 								'liferay-portlet-base'
 							]
 						},
-						'liferay-calendar-interval-selector': {
-							path: 'interval_selector.js',
-							requires: [
-								'aui-base',
-								'liferay-portlet-base'
-							]
-						},
-						'liferay-calendar-interval-selector-scheduler-event-link': {
-							path: 'interval_selector_scheduler_event_link.js',
-							requires: [
-								'aui-base',
-								'liferay-portlet-base'
-							]
-						},
-						'liferay-calendar-list': {
-							path: 'calendar_list.js',
-							requires: [
-								'aui-template-deprecated',
-								'liferay-scheduler'
-							]
-						},
-						'liferay-calendar-message-util': {
-							path: 'message_util.js',
-							requires: [
-								'aui-alert',
-								'liferay-util-window'
-							]
-						},
-						'liferay-calendar-recurrence-converter': {
-							path: 'recurrence_converter.js',
-							requires: []
-						},
-						'liferay-calendar-recurrence-dialog': {
-							path: 'recurrence.js',
-							requires: [
-								'aui-base',
-								'liferay-calendar-recurrence-util'
-							]
-						},
-						'liferay-calendar-recurrence-util': {
-							path: 'recurrence_util.js',
-							requires: [
-								'aui-base',
-								'liferay-util-window'
-							]
-						},
-						'liferay-calendar-reminders': {
-							path: 'calendar_reminders.js',
-							requires: [
-								'aui-base'
-							]
-						},
 						'liferay-calendar-remote-services': {
 							path: 'remote_services.js',
 							requires: [
@@ -79,37 +27,13 @@
 								'liferay-portlet-url'
 							]
 						},
-						'liferay-calendar-session-listener': {
-							path: 'session_listener.js',
-							requires: [
-								'aui-base',
-								'liferay-scheduler'
-							]
-						},
-						'liferay-calendar-simple-color-picker': {
-							path: 'simple_color_picker.js',
-							requires: [
-								'aui-base',
-								'aui-template-deprecated'
-							]
-						},
-						'liferay-calendar-simple-menu': {
-							path: 'simple_menu.js',
-							requires: [
-								'aui-base',
-								'aui-template-deprecated',
-								'event-outside',
-								'event-touch',
-								'widget-modality',
-								'widget-position',
-								'widget-position-align',
-								'widget-position-constrain',
-								'widget-stack',
-								'widget-stdmod'
-							]
-						},
-						'liferay-calendar-util': {
+						'liferay-calendar-util-override': {
 							path: 'calendar_util.js',
+							condition: {
+							   name: 'liferay-calendar-util-override',
+							   trigger: 'liferay-calendar-util',
+							   when: 'instead'
+							},
 							requires: [
 								'aui-datatype',
 								'aui-io',
@@ -120,8 +44,13 @@
 								'liferay-portlet-url'
 							]
 						},
-						'liferay-scheduler': {
+						'liferay-scheduler-override': {
 							path: 'scheduler.js',
+							condition: {
+								name: 'liferay-scheduler-override',
+								trigger: 'liferay-scheduler',
+								when: 'instead'
+							},
 							requires: [
 								'async-queue',
 								'aui-datatype',
@@ -138,6 +67,7 @@
 								'promise',
 								'resize-plugin'
 							]
+
 						},
 						'liferay-scheduler-event-recorder': {
 							path: 'scheduler_event_recorder.js',
